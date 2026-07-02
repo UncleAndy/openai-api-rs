be52d0037ee4e428922fc2f1f58e7590aef43aae
Andrey Velikoredchanin <andy@andyhost.ru>
2026-07-02 11:18:49 +0200

    Fix examples/chat_completion.rs for running as wasm32 (see comments).

--- examples/chat_completion.rs
+++ examples/chat_completion.rs
@@ -1,16 +1,58 @@
 use openai_api_rs::v1::api::OpenAIClient;
 use openai_api_rs::v1::chat_completion::chat_completion::ChatCompletionRequest;
 use openai_api_rs::v1::chat_completion::{self};
-use openai_api_rs::v1::common::GPT5_4;
 use std::env;
 
+/*
+    Run with GoogleAI in wasm32:
+
+    ```
+        cargo run --target wasm32-wasip2 --example chat_completion
+          --config 'target."wasm32-wasip2".runner="wasmtime run -S inherit-network -S http --env OPENAI_API_KEY=<API_KEY> --env OPENAI_API_BASE=https://generativelanguage.googleapis.com/v1beta/openai"'
+    ```
+ */
+
+#[cfg(not(all(target_arch = "wasm32", target_os = "wasi")))]
+use openai_api_rs::v1::common::GPT5_4;
+
+#[cfg(not(all(target_arch = "wasm32", target_os = "wasi")))]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let api_key = env::var("OPENAI_API_KEY").unwrap().to_string();
+    let api_key = env::var("OPENAI_API_KEY")?.to_string();
     let client = OpenAIClient::builder().with_api_key(api_key).build()?;
 
     let mut req = ChatCompletionRequest::new(
         GPT5_4.to_string(),
+        vec![chat_completion::ChatCompletionMessage {
+            role: chat_completion::MessageRole::user,
+            content: chat_completion::Content::Text(String::from("What is bitcoin?")),
+            name: None,
+            tool_calls: None,
+            tool_call_id: None,
+        }],
+    );
+
+    req.reasoning_effort = Some(chat_completion::ReasoningEffort::High);
+
+    let result = client.chat_completion(req).await?;
+    println!("Content: {:?}", result.inner.choices[0].message.content);
+
+    // print response headers
+    for (key, value) in result.headers.iter() {
+        println!("{}: {:?}", key, value);
+    }
+
+    Ok(())
+}
+
+#[cfg(all(target_arch = "wasm32", target_os = "wasi"))]
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_key = env::var("OPENAI_API_KEY")?.to_string();
+    let client = OpenAIClient::builder().with_api_key(api_key).build()?;
+
+    let mut req = ChatCompletionRequest::new(
+        "gemini-2.5-flash".to_string(),
         vec![chat_completion::ChatCompletionMessage {
             role: chat_completion::MessageRole::user,
             content: chat_completion::Content::Text(String::from("What is bitcoin?")),
